ea50cf94aa0c6e4aab97d734260887bcedf25a07
mkuchnia <52278081+mkuchnia@users.noreply.github.com>
2021-12-06 14:11:42 +0100

    Add files via upload

---
+++ Zadanie_0/zad0.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int Z;
+    cin >> Z;
+    for (int i = 0; i < Z; i++)
+    {
+        unsigned int liczba1, liczba2;
+        cin >> liczba1;
+        cin >> liczba2;
+        while (liczba1 != liczba2)
+        {
+            if (liczba1 < liczba2)
+                liczba2 = liczba2 - liczba1;
+            else
+                liczba1 = liczba1 - liczba2;
+        }
+        cout << liczba1 << endl;
+    }
+}
